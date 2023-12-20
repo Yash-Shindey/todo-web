5868d31cfe825a438d61161cfdf649c420aebf21
Yash Shindey <yashshindey1@gmail.com>
2023-12-21 00:58:48 +0530

    Working selenium and pyTest

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for
-
 app = Flask(__name__)
 tasks = []
 
@@ -20,4 +19,4 @@ def delete(index):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0', port=8080,debug=False)
